62efdc54404ee125899a8cbe54d1573b28c2487c
PriyamvadaP <priyam6575@gmail.com>
2023-09-23 13:05:43 +0530

    first commit

---
+++ Template/_blogs.php
@@ -0,0 +1,36 @@
+
+<!-- Blogs -->
+<section id="blogs">
+    <div class="container py-4">
+        <h4 class="font-rubik font-size-20">Popular Clubs</h4>
+        <hr>
+
+        <div class="owl-carousel owl-theme">
+            <div class="item">
+                <div class="card border-0 font-rale mr-5" style="width: 18rem;">
+                    <h5 class="card-title font-size-16">Eco Club</h5>
+                    <img src="./assets/blog/clubs.jpg" alt="cart image" class="card-img-top">
+                    <p class="card-text font-size-14 text-black-50 py-1">OIKOS, the Eco-Club of BMSIT&M functions under the Institutional Environmental Committee; and was conceptualised on 5th June 2016 (World Environment Day), and formally announced on 16th Sept 2016 (World Ozone Day), with vision & mission statements, bye-laws and a governing body</p>
+                    <a href="https://bmsit.ac.in/clubs-associations/eco-club#gsc.tab=0" class="color-second text-left">Know More</a>
+                </div>
+            </div>
+            <div class="item">
+                <div class="card border-0 font-rale mr-5" style="width: 18rem;">
+                    <h5 class="card-title font-size-16">Aero Club</h5>
+                    <img src="./assets/blog/club1.jpeg" alt="cart image" class="card-img-top">
+                    <p class="card-text font-size-14 text-black-50 py-1">The Aero club was launched on 5th May 2017 with a team of 13 core student members of 5th semester Mechanical Engineering..The Aero club was formed with an initiative to bring awareness about drone and aircraft building amongst the students.</p>
+                    <a href="https://bmsit.ac.in/clubs-associations/aero-club#gsc.tab=0" class="color-second text-left">Know More</a>
+                </div>
+            </div>
+            <div class="item">
+                <div class="card border-0 font-rale mr-5" style="width: 18rem;">
+                    <h5 class="card-title font-size-16">Coding Crib</h5>
+                    <img src="./assets/blog/blog3.jpg" alt="cart image" class="card-img-top">
+                    <p class="card-text font-size-14 text-black-50 py-1">Coding Crib is a student-run club started by a group of students who are coding enthusiasts in the year 2019. The Crib aims to promote coding among the students and also encourage analytical thinking</p>
+                    <a href="https://bmsit.ac.in/clubs-associations/coding-crib#gsc.tab=0" class="color-second text-left">Know More</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- !Blogs -->
